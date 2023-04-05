06dd709ff7c6f64796b1c2b6091fc604f334aeb8
Masanov Jumabek <masanov3167@gmail.com>
2023-04-05 23:26:32 +0500

    updated

--- src/pages/coffee.tsx
+++ src/pages/coffee.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import type { NextPage } from "next";
 import { Stack } from "@mui/system";
+import TopHero from "@/components/topHero";
 
 const CoffeePage: NextPage = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <TopHero
+        title="Enjoy your time in our bar"
+        bgImg="/images/coffeehero.png"
+        right
+        desc="The 1st Floristic Cafe & Bistro in Prague"
+      />
+    </Stack>
+  );
 };
 
 export default CoffeePage;
