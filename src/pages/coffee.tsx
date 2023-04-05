799943bfa35d7ba6d1c72a0f1360ee774cd481b3
Masanov Jumabek <masanov3167@gmail.com>
2023-04-05 22:41:08 +0500

    coffee page created

---
+++ src/pages/coffee.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import type { NextPage } from "next";
+import { Stack } from "@mui/system";
+
+const CoffeePage: NextPage = () => {
+  return <Stack>hello</Stack>;
+};
+
+export default CoffeePage;
